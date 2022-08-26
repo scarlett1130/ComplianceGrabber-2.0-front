04f4becab1b3c21138c46a8aaa7447639e5d395d
yahya benzha <yahya_benzha@um5.ac.ma>
2022-08-26 22:09:23 +0100

    fix issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import type { NextPage } from "next";
 
-
-
-const Navbar = ({ children }: LayoutProps) => {
-  return (
-    <div className="relative ">
-
-    </div>
-  );
+const Navbar = () => {
+  return <div className="relative "></div>;
 };
 
 export default Navbar;
