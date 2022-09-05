d356ff8896df1e00eb097d70624513006a6b44d8
yahya benzha <yahya_benzha@um5.ac.ma>
2022-09-06 00:00:54 +0100

    jljl

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
   return (
     <div className="absolute inset-0 max-h-screen ">
       <video
-        src="/animation.mp4"
+        src="/Animation.mp4"
         autoPlay
         muted
         loop
