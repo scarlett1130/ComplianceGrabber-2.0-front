76c65c0765c5978c7d339a969257a095f7640716
Vatler Lett <business.xfree2000@gmail.com>
2022-10-30 11:17:52 -0700

    update the numeral format

--- components/DashbordElements/Step4.tsx
+++ components/DashbordElements/Step4.tsx
@@ -11,6 +11,7 @@ import {
 import GetLiveData from "../../utils/GetLiveData";
 import Loader from "./Loader";
 import fileDownload from "js-file-download";
+var numeral = require('numeral');
 
 function step4() {
   const dispatch = useAppDispatch();
@@ -43,6 +44,12 @@ function step4() {
       fileDownload(DownloadableData, "Filled_Template.csv");
     }
   };
+  const DownloadNot = () => {
+    if (failedData) {
+      fileDownload(failedData, "NotFound_Template.csv");
+    }
+  };
+
 
   return (
     <div className="bg-gray-200 rounded-lg relative  flex flex-col justify-center items-center space-y-3 px-5 py-24 mt-9 ">
@@ -73,17 +80,31 @@ function step4() {
 
           <div className="">
             <p className="text-lg font-bold">
-              SPNs uploaded: {partnumbers.length}
+              SPNs uploaded: {numeral(partnumbers.length).format('0,0')}
             </p>
             <p className="text-lg font-bold">
-              Found: {successData}
+              Found: {numeral(successData).format('0,0')}
             </p>
             <p className="text-lg font-bold">
-              Not Found: {failedData.length}
+              Not Found: {numeral(failedData.length).format('0,0')}
             </p>
             {
-              failedData?.length > 0 && <p className="py-4 text-red-600">
+              failedData?.length > 0 && <p className="py-4 text-red-600 flex">
                 Not Found List:
+                <svg
+                  width="30"
+                  height="30"
+                  viewBox="0 0 96 64"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="mx-4 cursor-pointer"
+                  onClick={DownloadNot}
+                >
+                  <path
+                    d="M77.4 24.16C74.68 10.36 62.56 0 48 0C36.44 0 26.4 6.56 21.4 16.16C9.36 17.44 0 27.64 0 40C0 53.24 10.76 64 24 64H76C87.04 64 96 55.04 96 44C96 33.44 87.8 24.88 77.4 24.16ZM68 36L48 56L28 36H40V20H56V36H68Z"
+                    fill="red"
+                  />
+                </svg>
               </p>
             }
 
